17393a8e798087e30f82b7b1f15abb734645c96e
jefersonguerrajr <jefersonguerrajr@live.com>
2026-05-08 22:58:26 -0300

    feat: app para gerenciamento de contatos

---
+++ contatos-app/app/src/main/java/dev/jefersonguerrajr/contatos_app/model/Contato.kt
@@ -0,0 +1,17 @@
+package dev.jefersonguerrajr.contatos_app.model
+
+import java.time.LocalDate
+
+data class Contact(
+    val id: Long? = null,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val birthDate: LocalDate,
+    val cep: String,
+    val neighborhood: String,
+    val street: String,
+    val number: String,
+    val state: String,
+    val city: String
+)
